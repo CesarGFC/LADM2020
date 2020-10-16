5057f8b8e7e2001dad599e4b2a0a9494c129eeb8
Cesar Flores Carrillo <l16400906@morelia.tecnm.mx>
2020-10-15 23:10:58 -0600

    Diseño de pantallas en AndroidStudio

---
+++ app/src/main/java/mx/tenm/tepic/ladm_unidad1_practica1/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tenm.tepic.ladm_unidad1_practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
